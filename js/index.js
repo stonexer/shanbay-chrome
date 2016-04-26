a65cd57ab9c10326eec3ca4158c39f1ffc999bde
stonexer <shitianxin@gmail.com>
2016-04-26 17:59:52 +0800

    扇贝api，朗读

--- js/index.js
+++ js/index.js
@@ -11,17 +11,49 @@ loading = document.body.appendChild(loading)
 // 绑定点击事件
 var shanbayBox = document.createElement('div')
 shanbayBox.className = "shanbay-box"
+
+var boxTitle = document.createElement('div')
+boxTitle.className = "box-title"
+boxTitle = shanbayBox.appendChild(boxTitle)
+
+var boxDefinition = document.createElement('div')
+boxDefinition.className = "box-definition"
+boxDefinition = shanbayBox.appendChild(boxDefinition)
+
 shanbayBox = document.body.appendChild(shanbayBox)
 
 var mouseHandle = function(e) {
     var text = getSelectedText()
     
     if(text.replace(/(^\s*)|(\s*$)/g, "").length != 0) {
-        shanbayBox.innerHTML = text
+        if(text.length > 20 && text.indexOf(' ') > -1) {
+            return boxTitle.innerHTML = '目前只能测量单词哦'
+        }
+        boxTitle.innerHTML = text
         var pos = calcBoxPos(e)
         shanbayBox.style.top = pos.y + "px"
         shanbayBox.style.left = pos.x + "px"
         shanbayBox.style.display = 'block'
+        
+        fetch('https://api.shanbay.com/bdc/search/?word=' + text)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(json) {
+            var data = json.data
+            console.log(data)
+            boxDefinition.innerHTML = data.definition || '未找到定义'
+            
+            if(data.audio) {
+                var play = document.createElement('div')
+                play.className = "play"
+                play.onmouseover = function() {
+                    var audio = new Audio(data.audio)
+                    audio.play()
+                }
+                play = boxTitle.appendChild(play)
+            }
+        })
     } else {
         shanbayBox.style.display = 'none'
     }
